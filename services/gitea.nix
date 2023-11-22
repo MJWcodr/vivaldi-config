208da381b0cd3a216de2746cbed07c9bd33a0c64
Matthias Wünsch <matthias.wuensch@proton.me>
2023-11-22 22:22:56 +0100

    feature: Implement Gitea Actions

--- services/gitea.nix
+++ services/gitea.nix
@@ -1,12 +1,11 @@
 { config, pkgs, ... }:
 let
-	internalPort = 3000;
-	exposedPort = 3001;
-	domain = "vivaldi.fritz.box";
+  internalPort = 3000;
+  exposedPort = 3001;
+  domain = "vivaldi.fritz.box";
 
-	giteaRunnerDir = "/var/lib/gitea-runner/vivaldi";
-in
-{
+  giteaRunnerDir = "/var/lib/gitea-runner/vivaldi";
+in {
   ##########
   # Secrets
   ##########
@@ -24,19 +23,11 @@ in
       file = ../secrets/sslcert.key.age;
       owner = config.services.nginx.user;
     };
-		gitea-actions-token = {
-			file = ../secrets/gitea-actions-token.age;
-			owner = "gitea-runner";
-		};
+    gitea-ctions-token = {
+      file = ../secrets/gitea-actions-token.age;
+      owner = "gitea-runner";
+    };
   };
-	
-	# Create Gitea-Runner user
-	users.users.gitea-runner = {
-		isSystemUser = true;
-		name = "gitea-runner";
-		group = "gitea-runner";
-	};
-	users.groups.gitea-runner = {};
 
   ##########
   # Nginx
@@ -64,12 +55,11 @@ in
 
   services.postgresql = {
     ensureDatabases = [ config.services.gitea.user ];
-    ensureUsers = [
-      {
-        name = config.services.gitea.database.user;
-        ensurePermissions."DATABASE ${config.services.gitea.database.name}" = "ALL PRIVILEGES";
-      }
-    ];
+    ensureUsers = [{
+      name = config.services.gitea.database.user;
+      ensurePermissions."DATABASE ${config.services.gitea.database.name}" =
+        "ALL PRIVILEGES";
+    }];
   };
 
   ##########
@@ -77,12 +67,8 @@ in
   ##########
 
   system.activationScripts.gitea = {
-    text = ''
-      			#!/bin/sh
-      			set -e
-      			mkdir -p /srv/gitea
-      			chown -R ${config.services.gitea.user}:${config.services.gitea.group} /srv/gitea
-      		'';
+    text =
+      "	#!/bin/sh\n	set -e\n	mkdir -p /srv/gitea\n	chown -R ${config.services.gitea.user}:${config.services.gitea.group} /srv/gitea\n";
   };
 
   services.gitea = {
@@ -94,61 +80,77 @@ in
     };
     stateDir = "/srv/gitea";
     settings = {
-			server = {
-      	DOMAIN = domain;
-      	ROOT_URL = "https://${domain}:${toString exposedPort}/";
-      	HTTP_PORT = internalPort;
-    	};
-			service = {
-				DISABLE_REGISTRATION = true;
-			};
-			"service.explore" = {
-				REQUIRE_SIGNIN_VIEW = true;
-			};
-			actions.ENABLED = true;
-		};
+      server = {
+        DOMAIN = domain;
+        ROOT_URL = "https://${domain}:${toString exposedPort}/";
+        HTTP_PORT = internalPort;
+      };
+      service = { DISABLE_REGISTRATION = true; };
+      "service.explore" = { REQUIRE_SIGNIN_VIEW = true; };
+      actions.ENABLED = true;
+    };
   };
 
-	# Create a token for the actions runner
-	systemd.services.gitea-actions-runner-token = {
-		description = "Create a token for the gitea actions runner";
-		script = ''
-		token=$(${pkgs.sudo}/bin/sudo -u gitea gitea actions grt --config /srv/gitea/custom/conf/app.ini)
-		
-		mkdir -p $(dirname ${config.services.gitea-actions-runner.instances.vivaldi.tokenFile})
-		
-		echo $token > ${config.services.gitea-actions-runner.instances.vivaldi.tokenFile}
+  ##########
+  # Gitea Actions Runner
+  ##########
 
-		${pkgs.toybox}/bin/chown -R gitea-runner:  /var/lib/gitea-runner/vivaldi
+	# Create Gitea-Runner user
+  users.users.gitea-runner = {
+    isSystemUser = true;
+    name = "gitea-runner";
+    group = "gitea-runner";
+		extraGroups = [ "podman" ];
+  };
+  users.groups.gitea-runner = { };
+
+  # Enable Podman API
+  virtualisation.podman.dockerSocket.enable = true;
+
+  systemd.services.gitea-actions-runner-vivaldi-pre = {
+    description = "Gitea Actions Runner Setup for Vivaldi";
+    after = [ "gitea.service" ];
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = true;
+      User = "root";
+    };
+    path = [ pkgs.bash pkgs.coreutils pkgs.gitea pkgs.gitea-actions-runner pkgs.sudo ];
+    script = ''
+			token=$(sudo -u gitea gitea actions generate-runner-token --config ${config.services.gitea.stateDir}/custom/conf/app.ini)
+			mkdir -p ${giteaRunnerDir}	
+			
+			cd ${giteaRunnerDir}	
+			act_runner register	\
+				--instance "https://${domain}:${toString exposedPort}" \
+				--name vivaldi \
+				--token $token \
+				--labels "ubuntu-20.04:docker://node:16-bullseye, nixos:docker:nixos/nix:latest linux:host" \
+				--no-interactive 
+
+			chown -R gitea-runner:  ${giteaRunnerDir}'';
+  };
+		systemd.services.gitea-actions-runner-vivaldi = {
+    description = "Gitea Actions Runner for Vivaldi";
+    after = [ "gitea-actions-runner-vivaldi-pre.service" ];
+    path = [ pkgs.gitea-actions-runner ];
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      Type = "simple";
+      User = "gitea-runner";
+      WorkingDirectory = giteaRunnerDir;
+    };
+    script = ''
+			set -e
+			DOCKER_HOST="unix://$XDG_RUNTIME_DIR/podman/podman.sock"
+			act_runner daemon
 		'';
-		serviceConfig = {
-			Type = "oneshot";
-			RemainAfterExit = true;
-			User = "root";
-		};
-		path = [ 
-			pkgs.bash 
-			pkgs.coreutils 
-			pkgs.gitea 
-			];
-		after = [ "gitea.service" ];
-		wantedBy = [ "multi-user.target" ];
-	};
+  };
 
-
-	##########
-	# Gitea Actions Runner
-	##########
-	services.gitea-actions-runner.instances = {
-		"vivaldi" = {
-			enable = true;
-			url = config.services.gitea.settings.server.ROOT_URL;
-			name = "vivaldi";
-			labels = [ "native:host" ];
-			tokenFile = "/var/lib/gitea-runner/vivaldi/token";
-		};
-	};
-	
+  ##########
+  # Firewall
+  ##########
 
   networking.firewall.allowedTCPPorts = [ 3001 ];
 }
