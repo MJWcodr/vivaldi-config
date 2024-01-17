349a3c269572cf402648981d805138617a87f0d8
Matthias Wünsch <matthias.wuensch@proton.me>
2024-01-17 10:16:23 +0100

    feature: Add Minidlna

--- services/audio.nix
+++ services/audio.nix
@@ -4,8 +4,10 @@
   services.minidlna = {
     enable = true;
     settings = {
+			friendly_name = "NixOS Media Server";
       port = 8200;
       media_dir = [ "/srv/music" ];
+			inotify = "yes";
     };
     openFirewall = true;
   };
