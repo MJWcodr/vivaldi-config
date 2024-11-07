e4c6f758091e6ac2b785ddf9ed3017a9b51b8270
Matthias Wünsch <m.wuensch.extern@senec.com>
2024-11-07 21:42:16 +0100

    bugfix: fix path

--- hosts/vivaldi/configuration.nix
+++ hosts/vivaldi/configuration.nix
@@ -101,13 +101,14 @@ in {
       owner = config.services.nginx.user;
     };
   };
-  age.secrets = {
+
+	age.secrets = {
     "secrets/sslcert.crt.age" = {
-      file = ../../../secrets/sslcert.crt.age;
+      file = ../../secrets/sslcert.crt.age;
       owner = config.services.nginx.user;
     };
     "secrets/sslcert.key.age" = {
-      file = ../../../secrets/sslcert.key.age;
+      file = ../../secrets/sslcert.key.age;
       owner = config.services.nginx.user;
     };
   };
